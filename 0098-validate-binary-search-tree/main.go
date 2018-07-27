c73e6621c4e296b5bfe9ef2a202a14a856865294
Xargin <cao1988228@163.com>
2018-07-27 18:02:12 +0800

    add 98

--- 0098-validate-binary-search-tree/main.go
+++ 0098-validate-binary-search-tree/main.go
@@ -15,24 +15,50 @@ type TreeNode struct {
  *     Right *TreeNode
  * }
  */
+func isValidBSTWithBounds(root *TreeNode, lowerBound int, upperBound int) bool {
+	if root == nil {
+		return true
+	}
+
+	if (lowerBound != -1 && root.Val <= lowerBound) || (upperBound != -1 && root.Val >= upperBound) {
+		return false
+	}
+
+	leftUpperBound := root.Val
+	if upperBound != -1 && upperBound <= leftUpperBound {
+		leftUpperBound = upperBound
+	}
+
+	rightLowerBound := root.Val
+	if lowerBound != -1 && lowerBound >= rightLowerBound {
+		rightLowerBound = lowerBound
+	}
+
+	return isValidBSTWithBounds(root.Left, lowerBound, leftUpperBound) &&
+		isValidBSTWithBounds(root.Right, rightLowerBound, upperBound)
+
+}
 
 func isValidBST(root *TreeNode) bool {
 	if root == nil {
 		return true
 	}
 
+	// left
+	leftUpperBound := root.Val
 	if root.Left != nil {
-		if root.Left.Val > root.Val {
+		if !isValidBSTWithBounds(root.Left, -1, leftUpperBound) {
 			return false
 		}
 	}
 
+	// right
+	rightLowerBound := root.Val
 	if root.Right != nil {
-		if root.Right.Val < root.Val {
+		if !isValidBSTWithBounds(root.Right, rightLowerBound, -1) {
 			return false
 		}
 	}
 
-	return isValidBST(root.Left) && isValidBST(root.Right)
-
+	return true
 }
