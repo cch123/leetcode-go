58838c88587a388485833e070670d73e68a140f4
Xargin <cao1988228@163.com>
2018-08-24 10:46:06 +0800

    update heapsort

--- SORT/heapsort.go
+++ SORT/heapsort.go
@@ -2,17 +2,14 @@ package main
 
 import "fmt"
 
-func main() { arr := []int{4, 3, 2, 1}; hs(arr); fmt.Println(arr) }
+func main() { arr := []int{5, 4, 3, 2, 1}; hs(arr); fmt.Println(arr) }
 
 func hs(arr []int) {
-	if len(arr) == 0 || len(arr) == 1 {
-		return
-	}
 	buildHeap(arr)
 
-	for i := len(arr); i > 0; i-- {
-		arr[0], arr[i-1] = arr[i-1], arr[0]
-		adjustHeap(arr, 0, i-2)
+	for i := len(arr)-1; i >= 0; i-- {
+		arr[0], arr[i] = arr[i], arr[0]
+		adjustHeap(arr, 0, i-1)
 	}
 }
 
@@ -20,26 +17,23 @@ func buildHeap(arr []int) {
 	for i := len(arr) / 2; i >= 0; i-- {
 		// 调整以 arr[i] 为堆顶的堆
 		// left = 2*i+1, right = 2*i+2
-		// smallest := arr[i]
+		// biggest := arr[i]
 		adjustHeap(arr, i, len(arr)-1)
 	}
 }
 
 func adjustHeap(arr []int, minIdx int, maxIdx int) {
-	j := minIdx
+	i := minIdx
 	for {
-		smallest, idx := arr[j], j
-		if 2*j+1 <= maxIdx && arr[2*j+1] > smallest {
-			idx, smallest = 2*j+1, arr[2*j+1]
+		lIdx, rIdx, idx, max := 2*i+1, 2*i+2, i, arr[i]
+		if lIdx <= maxIdx && arr[lIdx] > max {
+			idx, max = lIdx, arr[lIdx]
 		}
-		if 2*j+2 <= maxIdx && arr[2*j+2] > smallest {
-			idx, smallest = 2*j+2, arr[2*j+1]
+		if rIdx <= maxIdx && arr[rIdx] > max {
+			idx, max = rIdx, arr[rIdx]
 		}
-		if idx == j {
-			break
-		}
-		arr[j], arr[idx] = arr[idx], arr[j]
-		j = idx
+		if idx == i {break}
+		arr[idx], arr[i] = arr[i], arr[idx]
 	}
 }
 
