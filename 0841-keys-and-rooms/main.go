a66e772e1874f36d8569ca52f9c6c4d0720b6d8a
Xargin <cao1988228@163.com>
2018-08-31 11:58:52 +0800

    add 841

--- 0841-keys-and-rooms/main.go
+++ 0841-keys-and-rooms/main.go
@@ -4,34 +4,36 @@ import "fmt"
 
 func main() {
 	//fmt.Println(canVisitAllRooms([][]int{[]int{1}, []int{2}, []int{3}, []int{}}))
-	fmt.Println(canVisitAllRooms([][]int{[]int{1, 3, 2}, []int{2, 3}, []int{2, 1, 3, 1}, []int{}}))
+	//fmt.Println(canVisitAllRooms([][]int{[]int{1, 3, 2}, []int{2, 3}, []int{2, 1, 3, 1}, []int{}}))
+	fmt.Println(canVisitAllRooms([][]int{[]int{1}, []int{}, []int{0, 3}, []int{1}}))
 }
 
 func canVisitAllRooms(rooms [][]int) bool {
-	for i := 0; i < len(rooms); i++ {
-		// start from rooms[i]
-		visited := map[int]bool{i: true}
-		nextVisit := rooms[i]
-		for {
-			newToVisit := []int{}
-			for j := 0; j < len(nextVisit); j++ {
-				if visited[nextVisit[j]] == true {
-					continue
-				}
-				visited[nextVisit[j]] = true
-				newToVisit = append(newToVisit, rooms[nextVisit[j]]...)
+	//for i := 0; i < len(rooms); i++ {
+	// start from rooms[i]
+	visited := map[int]bool{0: true}
+	nextVisit := rooms[0]
+	for {
+		newToVisit := []int{}
+		for j := 0; j < len(nextVisit); j++ {
+			if visited[nextVisit[j]] == true {
+				continue
 			}
-			if len(newToVisit) == 0 {
-				break
-			}
-			nextVisit = newToVisit
-			//fmt.Println(nextVisit)
+			visited[nextVisit[j]] = true
+			newToVisit = append(newToVisit, rooms[nextVisit[j]]...)
 		}
-
-		// check all
-		if len(visited) == len(rooms) {
-			return true
+		if len(newToVisit) == 0 {
+			break
 		}
+		nextVisit = newToVisit
+		//fmt.Println(nextVisit)
 	}
+
+	// check all
+	if len(visited) == len(rooms) {
+		//fmt.Println(visited, i)
+		return true
+	}
+	//}
 	return false
 }
